a10d6fe74150e09b216e3a7e4e0b21e9255e0b1c
bzayn02 <nagarkotibezay02@gmail.com>
2021-08-13 09:17:23 +1000

    react not-to-do

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Alert from 'react-bootstrap/Alert'
+
+function App() {
+  return (
+    <div>
+<Alert variant="primary">
+    Hello World
+  </Alert>
+    </div>
+   
+  );
+}
+
+export default App;
