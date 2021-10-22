19aab3b54e5ff0bd6af4cbfff4af10e455172306
bzay_n02 <nagarkotibezay02@gmail.com>
2021-10-22 13:37:33 +1100

    add ticket and fetch ticket apis integrated

--- src/App.js
+++ src/App.js
@@ -1,166 +1,183 @@
-import { useState } from "react";
-import { Container, Row, Col, Alert , Button} from "react-bootstrap";
-import { AddTaskForm } from "./components/form/AddTaskForm";
+import { useState, useEffect } from 'react';
+import { Container, Row, Col, Alert, Button, Spinner } from 'react-bootstrap';
+import { AddTaskForm } from './components/form/AddTaskForm';
 
-import { TaskList } from "./components/task-lists/TaskList";
-import { NotToDoList } from "./components/task-lists/NotToDoList";
+import { TaskList } from './components/task-lists/TaskList';
+import { NotToDoList } from './components/task-lists/NotToDoList';
+import { createTask, getTaskLists } from './apis/taskApi';
 
-import "./App.css";
+import './App.css';
 
 const hrPwk = 168;
 const App = () => {
-	const [tasks, setTasks] = useState([]);
-	const [badTasks, setBadTasks] = useState([]);
-	const [error, setError] = useState(false);
-	const [taskToDelete, setTaskToDelete]=useState([]);
-	const [badTaskToDelete, setBadTaskToDelete] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [badTasks, setBadTasks] = useState([]);
+  const [error, setError] = useState(false);
+  const [taskToDelete, setTaskToDelete] = useState([]);
+  const [badTaskToDelete, setBadTaskToDelete] = useState([]);
 
-	const taskHrs = tasks.reduce((subttl, itm) => subttl + +itm.hr, 0);
-	const badHours = badTasks.reduce((subttl, itm) => subttl + +itm.hr, 0);
-	const totalHrs = taskHrs + badHours;
+  const taskHrs = tasks.reduce((subttl, itm) => subttl + +itm.hr, 0);
+  const badHours = badTasks.reduce((subttl, itm) => subttl + +itm.hr, 0);
+  const totalHrs = taskHrs + badHours;
 
-	const addTaskList = frmDt => {
-		if (hrPwk < totalHrs + +frmDt.hr) {
-			setError(true);
-		} else {
-			error && setError(false);
-			setTasks([...tasks, frmDt]);
-		}
-	};
+  useEffect(() => {
+    // fetch all the tasks  and add to the lists.
+    const fetchingAllTask = async () => {
+      const { result } = await getTaskLists();
+      setTasks(result);
+    };
+    fetchingAllTask();
+  }, []);
 
-	//1. create a function and pass to task list component
-	//2. on button click, grab the index and pass the index to a function in the parent component
-	const markAsBadList = i => {
-		//3. take the task out of the task[] based on the index value we received
-		const tempTask = [...tasks];
-		const badTask = tempTask.splice(i, 1)[0];
-		//4. put the taken out task item to the badtask[]
-		setBadTasks([...badTasks, badTask]);
-		setTasks(tempTask);
-	};
+  const addTaskList = async (frmDt) => {
+    const result = await createTask(frmDt);
 
-	const markAsGoodList = i => {
-		const tempBadList = [...badTasks];
-		const goodTask = tempBadList.splice(i, 1)[0];
-		setTasks([...tasks, goodTask]);
-		setBadTasks(tempBadList);
-	};
+    if (result._id) {
+      //new task has been added successfully, now we can call api to fetch allteh data
 
+      console.log(result);
+    } else {
+      alert('unable to add teh task at the moment. please try again later.');
+    }
+  };
+  // sending form data to the server
 
-	//colelct indices of the task lsits  to be deleted.
+  // if (hrPwk < totalHrs + +frmDt.hr) {
+  // 	setError(true);
+  // } else {
+  // 	error && setError(false);
+  // 	setTasks([...tasks, frmDt]);
+  // }
 
-	const handleOnTaskClicked = e => {
-	
-		
-		const {checked, value} = e.target;
-		
-		if (checked){
-			setTaskToDelete([
-				 ...taskToDelete, +value
-			])
-		}else {
-				// const tempArg = [...taskToDelete]
-				// tempArg.splice(value, 1)
-				// setTaskToDelete(tempArg);
-				const filterArg = taskToDelete.filter ((item) => item!== +value)
-			setTaskToDelete(filterArg)
-			}
-			
-		};
+  //1. create a function and pass to task list component
+  //2. on button click, grab the index and pass the index to a function in the parent component
+  const markAsBadList = (i) => {
+    //3. take the task out of the task[] based on the index value we received
+    const tempTask = [...tasks];
+    const badTask = tempTask.splice(i, 1)[0];
+    //4. put the taken out task item to the badtask[]
+    setBadTasks([...badTasks, badTask]);
+    setTasks(tempTask);
+  };
 
-		//Delete item from task list only
-		const deleteFromTaskList = () => {
-			
-				const newArg = tasks.filter((item,i)=> !taskToDelete.includes(i))
-					
-				
-				setTaskToDelete([])
-				
-					setTasks(newArg)
-				}
+  const markAsGoodList = (i) => {
+    const tempBadList = [...badTasks];
+    const goodTask = tempBadList.splice(i, 1)[0];
+    setTasks([...tasks, goodTask]);
+    setBadTasks(tempBadList);
+  };
 
-				//from bad lsit
-				const deleteFromBadTaskList = () => {
-					
-						const newArg = badTasks.filter((item,i)=> !badTaskToDelete.includes(i))
-							
-						
-						setBadTaskToDelete([])
-						
-							setBadTasks(newArg)
-						}
-	
-//Delete a list from task lists and bad lists
-		const handleOnDeleteItems = () => {
-		
-			deleteFromTaskList();
-			deleteFromBadTaskList();
-		};
+  //colelct indices of the task lsits  to be deleted.
 
-		//list teh abd items indexx on checkbox click
-		const handleOnBadTaskClicked=(e)=>{
-			const {checked, value} =e.target
-			if(checked){
-				setBadTaskToDelete([
-					...badTaskToDelete, +value
-				])
-			}
-			else{
-const filterArg= badTaskToDelete.filter((item)=> item !== +value)
-setBadTaskToDelete(filterArg);
-			}
-		}
+  const handleOnTaskClicked = (e) => {
+    const { checked, value } = e.target;
 
-	return (
-		<div className="main">
-			<Container>
-				<Row>
-					<Col>
-						<h1 className="text-center mt-5">Not To-Do Task List</h1>
-					</Col>
-				</Row>
-				<hr />
-				<Row>
-					<Col>
-						{error && (
-							<Alert variant="danger">
-								!!! You can not add this task since it exceeds total hours in a week i.e. 168 hrs/week. 
-							</Alert>
-						)}
-					</Col>
-				</Row>
-				<AddTaskForm addTaskList={addTaskList} />
-				<hr />
-				<Row>
-					<Col>
-						<TaskList tasks={tasks} markAsBadList={markAsBadList} taskToDelete={taskToDelete}
-						handleOnTaskClicked={handleOnTaskClicked}/>
-					</Col>
-					<Col>
-						<NotToDoList
-							badTasks={badTasks}
-							markAsGoodList={markAsGoodList}
-							badHours={badHours}
-							handleOnBadTaskClicked={handleOnBadTaskClicked}
-						/>
-					</Col>
-				</Row>
-				
-				<Row className="py-3">
-					<Col>
-						<Button variant="danger" onClick={handleOnDeleteItems}>Delete</Button>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<Alert variant="info">
-							Your total allocated hours = {totalHrs} / 168 hours per week
-						</Alert>
-					</Col>
-				</Row>
-			</Container>
-		</div>
-	);
+    if (checked) {
+      setTaskToDelete([...taskToDelete, +value]);
+    } else {
+      // const tempArg = [...taskToDelete]
+      // tempArg.splice(value, 1)
+      // setTaskToDelete(tempArg);
+      const filterArg = taskToDelete.filter((item) => item !== +value);
+      setTaskToDelete(filterArg);
+    }
+  };
+
+  //Delete item from task list only
+  const deleteFromTaskList = () => {
+    const newArg = tasks.filter((item, i) => !taskToDelete.includes(i));
+
+    setTaskToDelete([]);
+
+    setTasks(newArg);
+  };
+
+  //from bad lsit
+  const deleteFromBadTaskList = () => {
+    const newArg = badTasks.filter((item, i) => !badTaskToDelete.includes(i));
+
+    setBadTaskToDelete([]);
+
+    setBadTasks(newArg);
+  };
+
+  //Delete a list from task lists and bad lists
+  const handleOnDeleteItems = () => {
+    deleteFromTaskList();
+    deleteFromBadTaskList();
+  };
+
+  //list teh abd items indexx on checkbox click
+  const handleOnBadTaskClicked = (e) => {
+    const { checked, value } = e.target;
+    if (checked) {
+      setBadTaskToDelete([...badTaskToDelete, +value]);
+    } else {
+      const filterArg = badTaskToDelete.filter((item) => item !== +value);
+      setBadTaskToDelete(filterArg);
+    }
+  };
+
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center mt-5">Not To-Do Task List</h1>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            {error && (
+              <Alert variant="danger">
+                !!! You can not add this task since it exceeds total hours in a
+                week i.e. 168 hrs/week.
+              </Alert>
+            )}
+          </Col>
+        </Row>
+        <AddTaskForm addTaskList={addTaskList} />
+        <hr />
+        <Row>
+          <Col>
+            {!tasks.length && !badTasks.length && (
+              <Spinner variant="info" animation="border" />
+            )}
+            <TaskList
+              tasks={tasks}
+              markAsBadList={markAsBadList}
+              taskToDelete={taskToDelete}
+              handleOnTaskClicked={handleOnTaskClicked}
+            />
+          </Col>
+          <Col>
+            <NotToDoList
+              badTasks={badTasks}
+              markAsGoodList={markAsGoodList}
+              badHours={badHours}
+              handleOnBadTaskClicked={handleOnBadTaskClicked}
+            />
+          </Col>
+        </Row>
+
+        <Row className="py-3">
+          <Col>
+            <Button variant="danger" onClick={handleOnDeleteItems}>
+              Delete
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Alert variant="info">
+              Your total allocated hours = {totalHrs} / 168 hours per week
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/apis/taskApi.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+const rootUrl = 'http://localhost:8000/';
+const taskApi = rootUrl + 'api/v1/';
+
+//post tasks
+export const createTask = (frmDt) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(taskApi, frmDt)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        resolve(false);
+      });
+  });
+};
+
+//fetch all tasks
+
+export const getTaskLists = () => {
+  return new Promise(async (resolve) => {
+    try {
+      const result = await axios.get(taskApi);
+      resolve(result.data);
+    } catch (error) {
+      console.log(error);
+      resolve(false);
+    }
+  });
+};
